1c73871945662e3ff612c698d68a46247a36ecf2
almarfeta <almareran@gmail.com>
2026-01-24 20:31:55 +0200

    Add OLAP indexes

---
+++ src/main/resources/db/olap/V3.0__create_indexes.sql
@@ -0,0 +1,16 @@
+CREATE BITMAP INDEX idx_month_year
+    ON dim_time (month, year);
+
+CREATE BITMAP INDEX idx_fs_status
+    ON fact_sales (status);
+
+-- Verify TODO: Modify this query to also include the second index (if possible)
+EXPLAIN PLAN FOR
+SELECT
+    fs.status,
+    SUM(fs.unit_price * fs.quantity) AS total_sales
+FROM fact_sales fs
+WHERE fs.status = 'ACCEPTED'
+GROUP BY fs.status;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
