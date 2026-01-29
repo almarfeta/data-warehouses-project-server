2de5baaf374c8ead396e354f085f5ad569a62bca
almarfeta <almareran@gmail.com>
2026-01-29 23:22:32 +0200

    Add dim_location partition and refactor index query

--- src/main/resources/db/olap/V3.0__create_indexes.sql
+++ src/main/resources/db/olap/V3.0__create_indexes.sql
@@ -5,17 +5,18 @@ CREATE BITMAP INDEX idx_fs_status
     ON fact_sales (status);
 
 EXPLAIN PLAN FOR
-SELECT
-    t.year,
-    t.month_name,
-    COUNT(*) AS number_of_sales,
+SELECT /*+ INDEX_COMBINE(f idx_fs_status) */
+    p.payment_method,
+    COUNT(f.sales_key) AS number_of_sales,
+    ROUND(AVG(f.unit_price * f.quantity), 2) AS sales_avg,
     SUM(f.unit_price * f.quantity) AS total_sales
 FROM fact_sales f
+JOIN dim_payment_method p ON f.payment_method_key = p.payment_method_key
 JOIN dim_time t ON f.time_key = t.time_key
-WHERE t.year = 2026
-  AND t.month = 1
-  AND f.status = 'ACCEPTED'
-GROUP BY t.year, t.month, t.month_name
-ORDER BY t.year, t.month;
+WHERE f.status = 'ACCEPTED'
+  AND t.year = 2026
+  AND t.month BETWEEN 1 AND 3
+GROUP BY p.payment_method
+ORDER BY total_sales DESC;
 
 SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
--- src/main/resources/db/olap/V5.0__create_partitions.sql
+++ src/main/resources/db/olap/V5.0__create_partitions.sql
@@ -6,4 +6,27 @@ ALTER TABLE fact_sales
         PARTITION p_future VALUES LESS THAN (MAXVALUE)
         );
 
--- TODO: Maybe add another partition for another table
+ALTER TABLE dim_location
+    MODIFY PARTITION BY LIST (country) (
+        PARTITION p_north_europe VALUES ('Finland'),
+        PARTITION p_west_europe VALUES ('Belgium', 'France', 'Netherlands'),
+        PARTITION p_central_europe VALUES ('Germany', 'Austria', 'Romania'),
+        PARTITION p_south_europe VALUES ('Spain', 'Italy', 'Greece'),
+        PARTITION p_america VALUES ('USA'),
+        PARTITION p_other VALUES (DEFAULT)
+        );
+
+EXPLAIN PLAN FOR
+SELECT
+    l.country,
+    t.month_name,
+    t.year,
+    SUM(f.unit_price * f.quantity) AS total_sales
+FROM fact_sales f
+JOIN dim_time t ON f.time_key = t.time_key
+JOIN dim_location l ON f.location_key = l.location_key
+WHERE f.time_key < 32
+  AND l.country = 'Romania'
+GROUP BY l.country, t.month_name, t.year;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
